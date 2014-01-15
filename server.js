7a5139c247fefae9cea33ee5054d29adf382f1e4
rhildred <rhildred@gmail.com>
2014-01-15 13:05:31 -0500

    added a complete very basic backbone example

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ assert = require('assert'),
 renderAsync = require('render-async'),
 renderFile = renderAsync.__express,
 jQuery = require('js-toolbox')._jQuery,
-johnyDrop = require('./js/johnyDrop.js').johnyDrop,
-marvelData = require('./js/marvelData.js').marvelData,
-insertStudent = require('./js/insertStudent.js').insertStudent,
-babySleep = require('./js/babySleep.js').babySleep;
+johnyDrop = require('./js/johnyDrop.js').johnyDrop;
 
 //now we need a server for this so that we can test include
 var app= renderAsync.express();
@@ -17,14 +14,6 @@ app.set('views', __dirname + '/public');
 // add a route for Johny Drop table
 
 app.get("/johnyDrop", function(req, res){johnyDrop(req, res);});
-
-//add a route for marvel 
-
-app.get("/marvelData", function(req, res){marvelData(req, res);});
-
-//add a route to insert a new student 
-
-app.post("/insertStudent", function(req, res){insertStudent(req, res);});
 
 //server everything index.html welcome file
 app.use(renderAsync.webServer);
